61a6befd7b39ebbcb692fc7d64068e6abf0877f6
Jordan Kaye <jordan.kaye2@gmail.com>
2024-03-06 07:44:01 -0700

    Nix flake support

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Thoughts on technical leadership and software engineering";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            devShells.default = import ./shell.nix { inherit pkgs; };
+          }
+      );
+}
--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") {}}:
+{ pkgs ? import (fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz"; sha256 = "0sjapzwnf4j41qqi9yj7fmbbafa9642zhfqdhp5mfyp14a6zj55b"; }) {}}:
 
 
 pkgs.mkShell {
+  name = "organizing-chaos";
   packages = [
     pkgs.hugo
   ];
