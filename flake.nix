0a844ec30d5ca9b0913f390295846dfcd58a75fd
Jordan Kaye <jordan.kaye2@gmail.com>
2024-03-30 16:44:08 +0000

    Adds Neovim and updates nix flake

--- flake.nix
+++ flake.nix
@@ -4,16 +4,18 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
+    neovim.url = "github:jkaye2012/neovim-flake";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, neovim }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          jkvim = neovim.packages.${system};
         in
           {
-            devShells.default = import ./shell.nix { inherit pkgs; };
+            devShells.default = import ./shell.nix { inherit pkgs jkvim; };
           }
       );
 }
--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
-{ pkgs ? import (fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz"; sha256 = "0sjapzwnf4j41qqi9yj7fmbbafa9642zhfqdhp5mfyp14a6zj55b"; }) {}}:
+{ pkgs ? import (fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz"; sha256 = "0sjapzwnf4j41qqi9yj7fmbbafa9642zhfqdhp5mfyp14a6zj55b"; }) {},
+  jkvim
+}:
 
 
 pkgs.mkShell {
   name = "organizing-chaos";
   packages = [
     pkgs.hugo
+    jkvim.jkvim
   ];
 }
