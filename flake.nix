161fa6c1b181a325c0eae0aab9182bced594db82
Jordan Kaye <jordan.kaye2@gmail.com>
2024-04-07 14:24:52 +0000

    Updates flake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     neovim.url = "github:jkaye2012/neovim-flake";
+    # neovim.url = "/home/jkaye/git/neovim-flake";
   };
 
   outputs = { self, nixpkgs, flake-utils, neovim }:
